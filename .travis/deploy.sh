35b2a28862105aadd0a5bbf9291f8d45e0fd0627
pandacode <the1ime@yandex.ru>
2020-10-28 22:59:08 +0500

    remove travis ci

--- .travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-git config --global push.default simple # we only want to push one branch — master
-# specify the repo on the live server as a remote repo, and name it 'production'
-# <user> here is the separate user you created for deploying
-git remote add production ssh://ubuntu@45.156.24.80/var/www/mc-server
-git push production master # push our updates
