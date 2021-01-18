4c47ff0b36602f495c4a37bfe08554561a8b73b9
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-01-18 17:29:55 +0500

    create SupportServices.getQuestion

--- services/support_services.ts
+++ services/support_services.ts
@@ -39,7 +39,7 @@ export default class SupportServices {
      * @param uid - user id
      * @param isUser - is user a patient or doctor
      * @param options - from & amount options
-     * @default options = { from: 0, amount: 50, limitQuestions: 50 }
+     * @default options = { from: 0, amount: 50, limitMessages: 50 }
      */
     public static getQuestions = async (uid: string, isUser: boolean, options: GetQuestionsType = {} ): Promise<SupportChat[]> => {
         const u = await (isUser ? User : Doctor).findById(uid)
@@ -56,13 +56,52 @@ export default class SupportServices {
         if (!u) throw "no_user_found";
 
         // slice messages
-        (u.chatsWithHelpers as SupportChat[] ?? []).forEach(
-            (e, i) => (u.chatsWithHelpers as SupportChat[])[i].messages = e.messages.slice(0, options.limitMessages ?? 50)
-        );
+        (u.chatsWithHelpers as SupportChat[]) = SupportServices.sliceManyMessage(u.chatsWithHelpers as SupportChat[] ?? [], options.limitMessages);
 
-        SupportServices._logger.i(`successfully get questions, id=${uid}, isUser=${isUser}, `, options, "length =", u.chatsWithHelpers?.length);
+        SupportServices._logger.i(`successfully get questions, id=${uid}, isUser=${isUser},`, options, "length =", u.chatsWithHelpers?.length);
         return (u.chatsWithHelpers as SupportChat[] ?? []);
     }
+
+    /**
+     * @throws "no_user_found"
+     * @throws "no_question_found"
+     * @param uid - user id
+     * @param questionId
+     * @param isUser - is user a patient or doctor
+     * @param options - from & amount options
+     * @default options = { from: 0, amount: 50, limitQuestions: 50 }
+     */
+    public static getQuestion = async (uid: string, questionId: string, isUser: boolean, options: GetQuestionType = {} ): Promise<SupportChat> => {
+        const u = await (isUser ? User : Doctor).findById(uid)
+            .populate({
+                path: "chatsWithHelpers",
+                match: { _id: questionId },
+                options: {
+                    limit: 1,
+                },
+            })
+            .select("chatsWithHelpers")
+            .lean();
+
+        if (!u) throw "no_user_found";
+        if (!u.chatsWithHelpers) throw "no_question_found";
+
+        // slice messages
+        (u.chatsWithHelpers[0] as SupportChat).messages = (u.chatsWithHelpers[0] as SupportChat).messages.slice(0, options.limitMessages ?? 50)
+
+        SupportServices._logger.i(`successfully get question, uid=${uid}, isUser=${isUser}, qid=${questionId}`, options, u.chatsWithHelpers[0]);
+        return u.chatsWithHelpers[0] as SupportChat;
+    }
+
+    private static sliceManyMessage = (chats: SupportChat[], limit: number = 50): SupportChat[] => {
+        return chats.map(
+            (e, i) => {
+                e.messages = e.messages.slice(0, limit);
+                return e;
+            }
+        );
+    }
+
 }
 
 type GetQuestionsType = { from?: number, amount?: number, limitMessages?: number };
